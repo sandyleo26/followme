1afcffa624d64cb7d924a74096e5fc4582c7e4c6
Sam Leo <sandyleo26@gmail.com>
2017-11-21 22:41:43 +1100

    augment data by flipping training data.

---
+++ train_aug.py
@@ -0,0 +1,21 @@
+import os
+import glob
+from scipy import misc
+import numpy as np
+
+## flip training data set.
+# shared by minimithi on Slack
+# https://medium.com/towards-data-science/image-augmentation-for-deep-learning-histogram-equalization-a71387f609b2
+# https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+def flip_and_save_images(img_dir, extension):
+  os.chdir(img_dir)
+  files = glob.glob("*." + extension)
+  for i, file in enumerate(files):
+    print(i)
+    img = misc.imread(file, flatten=False, mode='RGB')
+    flipped_img = np.fliplr(img)
+    misc.imsave("flipped_" + file, flipped_img)
+
+################
+flip_and_save_images("./data/train/masks", "png")
+flip_and_save_images("./data/train/images", "jpeg")
